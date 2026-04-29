b8f80a784145d21823753b354ab0033438961389
Иван Кириян <ivankirian@MacBook-Air-Ivan.local>
2026-04-29 05:14:53 +0300

    Реализован репозиторий сообщений

---
+++ app/repositories/chat_messages.py
@@ -0,0 +1,31 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, delete
+
+from app.db.models import ChatMessage
+
+class ChatMessageRepository:
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session #хранит как приватное поле
+
+    async def add(self, user_id: int, role: str, content: str) -> ChatMessage: #добавляет сообщение
+        message = ChatMessage(user_id=user_id, role=role, content=content)
+        self._session.add(message)
+        await self._session.commit()
+        await self._session.refresh(message)
+        return message
+    
+    async def get_nlast_message(self, user_id: int, n: int) -> list[ChatMessage]: #получает n последних сообщенией
+        result = await self._session.execute(
+            select(ChatMessage)
+            .where(ChatMessage.user_id == user_id)
+            .order_by(ChatMessage.created_at.desc())
+            .limit(n)
+        )
+        messages = result.scalars().all()
+        return list(reversed(messages))
+    
+    async def clear_history(self, user_id: int) -> None: #удаляет историю
+        await self._session.execute(
+            delete(ChatMessage).where(ChatMessage.user_id == user_id)
+        )
+        await self._session.commit()
